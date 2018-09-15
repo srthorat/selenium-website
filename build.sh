9c876f4facc3c3e871420a6e44c41145d2b72d16
Sakharam Thorat <sakharam.thorat@greenwavesystems.com>
2018-09-16 01:33:43 +0800

    Modified build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo docker build ./mysql/ -t db
-sudo docker run -it -d -p 3306:3306 --name mysql db
+docker build ./mysql/ -t db
+docker run -it -d -p 3306:3306 --name mysql db
 
-sudo docker build ./website/ -t web
-sudo docker run -it -d -p 3001:80 --link mysql:db web
+docker build ./website/ -t web
+docker run -it -d -p 3001:80 --link mysql:db web
